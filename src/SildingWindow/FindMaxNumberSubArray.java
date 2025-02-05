28b2bee29306d6d16c3a1dc66051488be017dfc4
Pankaj Harit <pankajharit46@gmail.com>
2025-02-06 01:00:20 +0530

    finding maximum element in a sub array

---
+++ src/SildingWindow/FindMaxNumberSubArray.java
@@ -0,0 +1,41 @@
+package SildingWindow;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class FindMaxNumberSubArray {
+    public static void main(String[] args) {
+        List<Integer> result = findMaxNUmbrSubArray(new int[]{
+                1, 3, 2, 1, 5, 4
+        }, 3);
+        System.out.println(result);
+    }
+
+    public static List<Integer> findMaxNUmbrSubArray(int[] arr, int window) {
+
+        List<Integer> result = new ArrayList<>();
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        int start = 0, end = 0;
+        while (end < arr.length) {
+            //remove all the smallest element from the last
+            while (!queue.isEmpty() && queue.peekLast() < arr[end])  queue.removeLast();
+            queue.add(arr[end]);
+
+            if (end - start + 1 == window) {
+
+                int firstElement = queue.peekFirst();
+                result.add(firstElement);
+
+                if (arr[start] == queue.peekFirst()) queue.removeFirst();
+
+                start++;
+            }
+
+            end++;
+        }
+        return result;
+    }
+}
